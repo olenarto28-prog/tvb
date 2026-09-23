a721634a74f13bee6ec03f20e0178978cd936ed0
olenarto28-prog <olenarto28@gmail.com>
2026-09-22 21:27:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,13 @@ const config = {
     ping_timeout: 60
   },
   http: {
-    port: process.env.PORT || 8000, // Usa a porta da hospedagem online
-    allow_origin: '*' // Permite que o seu player web acesse o vídeo sem travar
+    port: process.env.PORT || 8000,
+    allow_origin: '*'
   },
-  trans: {
-    ffmpeg: '/usr/bin/ffmpeg', // Caminho padrão do FFmpeg no servidor Linux online
-    tasks: [
-      {
-        app: 'live',
-        hls: true,
-        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-        dash: false
-      }
-    ]
+  auth: {
+    api: true,
+    api_user: 'admin',
+    api_pass: 'admin',
   }
 };
 
